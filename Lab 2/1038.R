e0c83dffe433c197420fe317b666727410286ecd
nawodyaishan <nawodyain@gmail.com>
2022-09-30 20:46:53 +0530

    Add Lab 2

---
+++ Lab 2/1038.R
@@ -0,0 +1,84 @@
+# Installing Packages
+install.packages("e1071")
+install.packages("caTools")
+install.packages("class")
+
+# Loading package
+library(e1071)
+library(caTools)
+library(class)
+
+# Load the Iris data set
+data <- read.table(file.choose(FALSE), header = T, sep = ",", dec = ".")
+
+# Check Top observations present in the data
+head(data)
+
+# Check the dimensions of the data
+dim(data)
+
+#Summarise the data
+summary(data)
+
+#Splitting the data set into train and test
+split <- sample.split(iris, SplitRatio = 0.7)
+train_cl <- subset(iris, split == "TRUE")
+test_cl <- subset(iris, split == "FALSE")
+
+# Feature Scaling
+train_scale <- scale(train_cl[, 1:4])
+test_scale <- scale(test_cl[, 1:4])
+
+# Fitting KNN Model
+# to training dataset
+classifier_knn <- knn(train = train_scale,
+                      test = test_scale,
+                      cl = train_cl$Species,
+                      k = 1)
+classifier_knn
+
+
+# Confusiin Matrix
+cm <- table(test_cl$Species, classifier_knn)
+cm
+
+# Model Evaluation - Choosing K
+# Calculate out of Sample error
+misClassError <- mean(classifier_knn != test_cl$Species)
+print(paste('Accuracy =', 1-misClassError))
+
+# K = 3
+classifier_knn <- knn(train = train_scale,
+                      test = test_scale,
+                      cl = train_cl$Species,
+                      k = 3)
+misClassError <- mean(classifier_knn != test_cl$Species)
+print(paste('Accuracy =', 1-misClassError))
+
+# K = 5
+classifier_knn <- knn(train = train_scale,
+                      test = test_scale,
+                      cl = train_cl$Species,
+                      k = 5)
+misClassError <- mean(classifier_knn != test_cl$Species)
+print(paste('Accuracy =', 1-misClassError))
+
+# K = 15
+classifier_knn <- knn(train = train_scale,
+                      test = test_scale,
+                      cl = train_cl$Species,
+                      k = 15)
+misClassError <- mean(classifier_knn != test_cl$Species)
+print(paste('Accuracy =', 1-misClassError))
+
+# K = 19
+classifier_knn <- knn(train = train_scale,
+                      test = test_scale,
+                      cl = train_cl$Species,
+                      k = 19)
+misClassError <- mean(classifier_knn != test_cl$Species)
+print(paste('Accuracy =', 1-misClassError))
+
+
+
+
